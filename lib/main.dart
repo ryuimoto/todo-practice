19a7dcb8430dbdf2298f2f56353a80acab1122d5
maca <narrow617@gmail.com>
2022-03-24 23:25:01 +0900

    add appbar

--- lib/main.dart
+++ lib/main.dart
@@ -19,12 +19,53 @@ class MyTodoApp extends StatelessWidget{
   }
 }
 
+
+// リスト一覧画面用Widget
 class TodoListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('リスト一覧'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Text('ニンジンを買う'),
+          Text('玉ねぎを買う'),
+          Text('ジャガイモを買う'),
+          Text('カレールーを買う'),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // "push"で新規画面に遷移
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              // 遷移先の画面としてリスト追加画面を指定
+              return TodoAddPage();
+            }),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+// リスト追加画面用Widget
+class TodoAddPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       body: Center(
-        child: Text('リスト一覧画面'),
+        child: TextButton(
+          // ボタンをクリックした時の処理
+          onPressed: () {
+            // "pop"で前の画面に戻る
+            Navigator.of(context).pop();
+          },
+          child: Text('リスト追加画面（クリックで戻る）'),
+        ),
       ),
     );
   }
